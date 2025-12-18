3cc94808ce7b4a7cdab3e5f050331ec86d4f3b19
Sevims Office <121771333+SevimsOffice@users.noreply.github.com>
2025-12-18 20:57:02 +0300

    Remove CNAME to use GitHub Pages URL

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/SGTohumculuk/",
+  base: "/SG-Tohumculuk/",
   server: {
     host: "::",
     port: 8080,
